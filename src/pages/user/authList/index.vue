9798efb3eac179067addcf1c816bfce60045abf2
宋锦法 <1778775545@qq.com>
2023-03-03 16:43:27 +0800

    基础页面配置

---
+++ src/pages/user/authList/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        权限配置页面
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,9 +12,41 @@ const routes: RouteRecordRaw[] = [
     },
   },
   {
+    path: "/user",
+    name: "User",
+    component: () => import("../pages/user/userList/index.vue"),
+    meta: {
+      title: ["用户模块", "用户列表"],
+    },
+  },
+  {
+    path: "/role",
+    name: "Role",
+    component: () => import("../pages/user/roleList/index.vue"),
+    meta: {
+      title: ["用户模块", "角色列表"],
+    },
+  },
+  {
+    path: "/authority",
+    name: "Authority",
+    component: () => import("../pages/user/authList/index.vue"),
+    meta: {
+      title: ["用户模块", "权限配置"],
+    },
+  },
+  {
+    path: "/questionSort",
+    name: "QuestionSort",
+    component: () => import("../pages/questionBank/questionSelect/index.vue"),
+    meta: {
+      title: ["题库管理", "题库分类"],
+    },
+  },
+  {
     path: "/questionList",
     name: "QuestionList",
-    component: () => import("../pages/questionBank/index.vue"),
+    component: () => import("../pages/questionBank/questList/index.vue"),
     meta: {
       title: ["题库管理", "题库列表"],
     },
